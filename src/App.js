6855e072da276135292f0cc91f8a72dc8e50cdfa
Antony PAQUAY <apaquay@adneom.com>
2019-11-25 21:23:26 +0100

    ci(): add message animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import "./App.css";
+import "./animations.css";
 import Formulaire from "./components/Formulaire";
 import Message from "./components/Message";
+// firebase
 import base from "./base";
+// Animations
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends Component {
   state = {
@@ -10,33 +14,55 @@ class App extends Component {
     pseudo: this.props.match.params.pseudo
   };
 
+  messagesRef = createRef();
+
   componentDidMount() {
-    base.syncState('/', {
+    base.syncState("/", {
       context: this,
-      state: 'messages'
-    })
+      state: "messages"
+    });
+  }
+
+  componentDidUpdate() {
+    const ref = this.messagesRef.current;
+    ref.scrollTop = ref.scrollHeight;
   }
 
   addMessage = message => {
     const messages = { ...this.state.messages };
     messages[`message-${Date.now()}`] = message;
+    Object.keys(messages)
+      .slice(0, -10)
+      .forEach(key => {
+        messages[key] = null;
+      });
     this.setState({ messages });
   };
+
+  isUser = pseudo => pseudo === this.state.pseudo;
 
   render() {
     const messages = Object.keys(this.state.messages).map(key => {
       return (
-        <Message
+        <CSSTransition 
           key={key}
-          message={this.state.messages[key].message}
-          pseudo={this.state.messages[key].pseudo}
-        />
+          timeout={200}
+          classNames='fade'
+        >
+          <Message
+            isUser={this.isUser}
+            message={this.state.messages[key].message}
+            pseudo={this.state.messages[key].pseudo}
+          />
+        </CSSTransition>
       );
     });
 
     return (
       <div className="box">
-        <div className="messages">{messages}</div>
+        <div className="messages" ref={this.messagesRef}>
+          <TransitionGroup className="message">{messages}</TransitionGroup>
+        </div>
         <Formulaire
           length={140}
           pseudo={this.state.pseudo}
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Message = ({pseudo, message}) => {
-return <p className="user-message">{`${pseudo.toUpperCase()}: ${message}`}</p>;
+const Message = ({pseudo, message, isUser}) => {
+    if(isUser(pseudo)){
+    return <p className="user-message"><strong>{pseudo}: </strong>{message}</p>;
+    } else {
+        return <p className="not-user-message"><strong>{pseudo}: </strong>{message}}</p>;
+    }
+
 };
 
 export default Message;
